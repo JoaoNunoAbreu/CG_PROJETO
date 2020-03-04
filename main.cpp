1b5e6071fabb5e84cb54e4d572aa225b4b04c873
João Nuno Abreu <joao.nuno.escola@gmail.com>
2020-03-04 19:19:53 +0000

    esfera feita, falta cone e dividir cubo

--- main.cpp
+++ main.cpp
@@ -66,6 +66,8 @@ void drawPlane(float x, float z) {
         glVertex3f(-x/2, 0, z/2);
         glVertex3f(x/2, 0, z/2);
     
+        glColor3f(1,0.5,1);
+    
         glVertex3f(-x/2, 0, -z/2);
         glVertex3f(x/2, 0, z/2);
         glVertex3f(x/2, 0, -z/2);
@@ -155,25 +157,122 @@ void drawCube(float x, float y, float z) {
 
 }
 
-// TEMPORÁRIO
-void drawCircle(float r, int slices){
-    float angulo = (2*M_PI) /slices;
+void drawSphere(float radius, int slices, int stacks){
+    float angulo_camada = (float)(M_PI / stacks);
+    float angulo_fatia = (float)(2 * M_PI / slices);
+    float beta = (float)-(M_PI / 2.0f);
+    float alpha = 0;
+    float nextBeta = beta + angulo_camada;
+    float nextAlpha = alpha + angulo_fatia;
+    
+    float xA, yA, zA;        //Ponto A - canto superior esquerdo
+    float xB, yB, zB;        //Ponto B - canto inferior esquerdo
+    float xC, yC, zC;        //Ponto C - canto superior direito
+    float xD, yD, zD;        //Ponto D - canto inferior direito
+
     glBegin(GL_TRIANGLES);
-        for(int i = 0; i < slices; i++){
-            glColor3f((1./slices)*i,0,1);
-            glVertex3f(0,0,0);
-            glVertex3f(r*sin(angulo*i),0,r*cos(angulo*i));
-            glVertex3f(r*sin(angulo*(i+1)),0,r*cos(angulo*(i+1)));
+    for (int stack = 0; stack < stacks; stack++) {
+        for (int slice = 0; slice < slices; slice++) {
+
+            xA= radius * cos(nextBeta) * sin(alpha);
+            yA= radius * sin(nextBeta);
+            zA= radius * cos(nextBeta) * cos(alpha);
+            
+            xB = radius * cos(beta) * sin(alpha);
+            yB = radius * sin(beta);
+            zB = radius * cos(beta) * cos(alpha);
+
+            xC = radius * cos(nextBeta) * sin(nextAlpha);
+            yC = radius * sin(nextBeta);
+            zC = radius * cos(nextBeta) * cos(nextAlpha);
+
+            xD = radius * cos(beta) * sin(nextAlpha);
+            yD = radius * sin(beta);
+            zD = radius * cos(beta) * cos(nextAlpha);
+            
+            glColor3f((1./slices)*slice,0,1);
+            glVertex3f(xC, yC, zC);
+            glVertex3f(xA, yA, zA);
+            glVertex3f(xB, yB, zB);
+            
+            glColor3f((1./stacks)*stack,0,1);
+            glVertex3f(xB, yB, zB);
+            glVertex3f(xD, yD, zD);
+            glVertex3f(xC, yC, zC);
+
+            alpha = nextAlpha;
+            nextAlpha += angulo_fatia;
         }
+        beta = nextBeta;
+        nextBeta += angulo_camada;
+    }
     glEnd();
 }
 
-void drawSphere(float radius, int slices, int stacks){
+void drawCone(float radius, float height, int slices, int stacks){
+    glBegin(GL_TRIANGLES);
     
-}
+        float angle = 0;                // Ângulo ao centro
+        float alfa = (2 * M_PI) / ((float) slices);
+        float diffRadius = (radius/stacks); // Diferença entre valores de raio de cada stack
 
-void drawCone(float r, float height, int slices, int stacks){
+        // Coordenadas de referência
+        float lowerR = 0;        // Raio do círculo dos 2 pontos inferiores
+        float upperR = 0;        // Raio do círculo dos 2 pontos superiores
+
+        // Pontos de referência para aplicar-se a regra da mão direita
+        float xA, yA, zA;        //Ponto A - canto inferior esquerdo
+        float xB, yB, zB;        //Ponto B - canto inferior direito
+        float xC, yC, zC;        //Ponto C - canto superior direito
+        float xD, yD, zD;        //Ponto D - canto superior esquerdo
+
+        float ha = sqrt(pow(radius,2) + pow(height,2))/stacks;
     
+        for (int i = 0; i < stacks; i++) {
+
+            lowerR = radius-(diffRadius * i);
+            upperR = radius-(diffRadius * (i+1));
+            
+            // Percorrem-se as slices e calculam-se as coordenadas
+            // dos pontos de referência para a stack e slice atuais
+            for (int j = 0; j < slices; j++) {
+                angle = alfa * j;
+
+                // Cálculo das novas coordenadas do ponto A
+                xA = lowerR * sin(angle);
+                yA = ha * i;
+                zA = lowerR * cos(angle);
+
+
+                // Cálculo das novas coordenadas do ponto B
+                xB = lowerR * sin(angle + alfa);
+                yB = ha * i;
+                zB = lowerR * cos(angle + alfa);
+
+                // Cálculo das novas coordenadas do ponto C
+                xC = upperR * sin(angle + alfa);
+                yC = ha * (i+1);
+                zC = upperR * cos(angle + alfa);
+
+                // Cálculo das novas coordenadas do ponto D
+                xD = upperR * sin(angle);
+                yD = ha * (i+1);
+                zD = upperR * cos(angle);
+
+
+                // Inserção dos pontos na estrutura com os resultados
+                glColor3f((1./slices)*i,0,1);
+                glVertex3f(xA, yA, zA);
+                glVertex3f(xB, yB, zB);
+                glVertex3f(xC, yC, zC);
+
+                glColor3f((1./stacks)*j,0,1);
+                glVertex3f(xA, yA, zA);
+                glVertex3f(xC, yC, zC);
+                glVertex3f(xD, yD, zD);
+            }
+        }
+    glEnd();
 }
 
 void renderScene(void) {
@@ -189,11 +288,12 @@ void renderScene(void) {
     
     drawAxis();
     glRotatef(angle1,0,1,0);
-    glRotatef(angle2,0,0,1);
+    glRotatef(angle2,1,0,0);
+    //drawPlane(2, 2);
     //drawCube(2,2,2);
-    //drawSphere(2, 4, 4);
-    drawCircle(1, 20);
-    //drawCone(2,3,50,50);
+    //drawSphere(2, 100, 100);
+    drawCone(2,2,8,100);
+    //glutWireCone(2, 2, 100, 2);
     
     // End of frame
     glutSwapBuffers();
