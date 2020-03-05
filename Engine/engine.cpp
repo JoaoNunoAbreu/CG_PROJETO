6e737b9d43befe50adcbb486099119a0ae31cf36
João Nuno Abreu <joao.nuno.escola@gmail.com>
2020-03-05 23:02:48 +0000

    text

--- Engine/engine.cpp
+++ Engine/engine.cpp
@@ -23,7 +23,7 @@ float radius = 5;
 float alpha = 0;
 float beta = 0;
 
-void renderScene(void) {
+/*void renderScene(void) {
 
     // clear buffers
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -64,21 +64,48 @@ void processSpecialKeys(int key, int xx, int yy) {
 
 }
 
+static string getElementText(XMLElement *_element) {
+    string value;
+    if(_element != NULL) {
+        value = _element->GetText();
+    }
+      
+    return value;
+}*/
+
 void readXML(string fich) {
     
-    XMLDocument doc;
-    XMLElement *root;
+    static const char* xml =
+    "<?xml version=\"1.0\" encoding=\"utf-8\" ?>"
+    "<entries>"
+        "<entry name=\"My First Post\" age=\"52\">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine</entry>"
+        "<entry name=\"The Second\" age=\"\">You know, being a test pilot isn't always the healthiest business in the world.</entry>"
+        "<entry>Entry</entry>"
+        "<entry name=\"The Third\" secretdata=\"9809832\">We have an infinite amount to learn both from nature and from each other</entry>"
+        "<entry name=\"Final Post...\" hidden=\"true\" age=\"3\">Across the sea of space, the stars are other suns.</entry>"
+    "</entries>";
 
-    if (!(doc.LoadFile(fich.c_str()))) {
-        root = doc.FirstChildElement();
-        for (XMLElement *elem = root->FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement()) {
-            string ficheiro = elem->Attribute("file");
-            cout << "Ficheiro:" << ficheiro << "lido com sucesso" << endl;
-            //readFile(ficheiro);
+    XMLDocument doc;
+    doc.Parse(xml);
+    
+    XMLHandle docHandle(&doc);
+    
+    XMLElement *entry = docHandle.FirstChildElement("entries").ToElement();
+    
+    if(entry){
+        for(XMLNode *node = entry->FirstChildElement(); node; node = node->NextSibling()){
+            XMLElement *e = node->ToElement();
+
+            const char *name = e->Attribute("name");
+            if(name) cout<<name<<": ";
+            
+            cout<<e->GetText();
+            
+            int true_age = e->IntAttribute("age") + 50;
+            
+            cout<<" "<<true_age <<endl;
+            }
         }
-    } else {
-        cout << "Ficheiro XML não foi encontrado" << endl;
-    }
 }
 
 int main(){
