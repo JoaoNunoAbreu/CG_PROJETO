2a22dbdb8247db6f942c45a1ca10027cfa58accc
TiagoMag <tiagohomagalhaes@gmail.com>
2020-04-29 16:16:35 +0100

    fase2

--- Aula5/build/class5.build/Debug/ZERO_CHECK.build/Script-3384F359615C4ECA8E0D99B5.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/joaonunoabreu/Desktop/2ºSemestre/CG/Aula5/build -f /Users/joaonunoabreu/Desktop/2ºSemestre/CG/Aula5/build/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
